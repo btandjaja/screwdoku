4a9669d7acf6ffc07c2058ad848673a662bc0770
App Academy Student <student@appacademy.io>
2017-01-26 11:38:13 -0800

    level-1 fix

--- sudoku.rb
+++ sudoku.rb
@@ -12,7 +12,7 @@ class SudokuGame
 
   def get_pos
     pos = nil
-    raise "This is the easiest error you're gonna get, chief.  It only gets harder going forward.  Remove me (or comment me out), re-run the code to make sure it works, and go on to the next level"
+    #raise "This is the easiest error you're gonna get, chief.  It only gets harder going forward.  Remove me (or comment me out), re-run the code to make sure it works, and go on to the next level"
 
     until pos && valid_pos?(pos)
       puts "Please enter a position on the board (e.g., '3,4')"
@@ -80,6 +80,7 @@ class SudokuGame
   attr_reader :board
 end
 
-
-game = SudokuGame.from_file("puzzles/sudoku1.txt")
-game.run
+if $0 == __FILE__
+  game = SudokuGame.from_file("puzzles/sudoku1.txt")
+  game.run
+end
